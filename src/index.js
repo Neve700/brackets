082a0a4f4c0910e8538cbf03a0fc741e666fc01a
Neve700 <147994506+Neve700@users.noreply.github.com>
2024-08-14 22:12:42 +0300

    fix: fix bracketMap and semicolons

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ module.exports = function check(str, bracketsConfig) {
   const splitStr = str.split('');
   let stack = [];
 
-  const bracketMap = {}
+  const bracketMap = {};
   for (const [open, close] of bracketsConfig) {
     bracketMap[open] = close;
   }
 
-  for(let i = 0; i < splitStr.length; i++) {
+  for (let i = 0; i < splitStr.length; i++) {
     const currentChar = splitStr[i];
 
-    if (bracketsMap[currentChar] {
-        if (currentChar === bracketsMap[currentChar] && stack.includes(currentChar)) {
-          stack.pop(); 
-        } else {
-          stack.push(currentChar);
-        }
+    if (bracketMap[currentChar]) { 
+      if (currentChar === bracketMap[currentChar] && stack.includes(currentChar)) {
+        stack.pop(); 
+      } else {
+        stack.push(currentChar);
+      }
     } else {
       const lastOpen = stack.pop();
-      if (bracketsMap[lastOpen] !== currentChar) {
+      if (bracketMap[lastOpen] !== currentChar) {
         return false;
       }
-    }          
+    }      
   }
-return stack.length === 0;
-};
+  return stack.length === 0;
+}
