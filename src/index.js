c19f09eafb9d4f3f33afd7cc3cefc3eef7f4652f
Neve700 <147994506+Neve700@users.noreply.github.com>
2024-08-14 22:03:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const splitStr = str.split('');
+  let stack = [];
+
+  const bracketMap = {}
+  for (const [open, close] of bracketsConfig) {
+    bracketMap[open] = close;
+  }
+
+  for(let i = 0; i < splitStr.length; i++) {
+    const currentChar = splitStr[i];
+
+    if (bracketsMap[currentChar] {
+        if (currentChar === bracketsMap[currentChar && stack.includes(currentChar)) {
+          stack.pop(); 
+        } else {
+          stack.push(currentChar);
+        }
+    } else {
+      const lastOpen = stack.pop();
+      if (bracketsMap[lastOpen] !== currentChar) {
+        return false;
+      }
+    }          
+  }
+return stack.length === 0;
+};
